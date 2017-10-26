5381fbbf8d9bd9ea0aef8d9b1984deb7c05b4176
Alexleslie <577045156@qq.com>
2017-10-26 13:27:33 +0800

    bug

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
+
+
+
 
 
 
@@ -75,13 +79,17 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
+if 'HeroKu' in os.environ:
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config()
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
